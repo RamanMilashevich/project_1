4088aeea4015f052686f12013e7b605478ba7fcd
Raman <roma.milashevich@gmail.com>
2023-09-26 14:52:22 +0200

    Practics №3, Use function 26.09.2023, 14.52

--- src/js/script.js
+++ src/js/script.js
@@ -159,7 +159,158 @@ str.length - и получить её длину)
 
 
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+
+// циклы - for / while 
+// for in 
+// for of
+
+// let num = 20;
+
+//  function showFirstMessage(text) {
+//     console.log(text);
+//     let num = 10;
+//     console.log(num);
+//  }
+
+//  showFirstMessage("Hello World!");
+// console.log(num);
+
+// function calc(a,b) {
+//     return (a + b);
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+// function ret() {
+//     let num = 50;
+//     return num;
+// }
+ 
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// FUNCTION EXPRESSION - let less = function() {}; - Создаётся только тогда когда доходит поток кода,  можно вызвать только после объявленияю
+
+// const logger = function() {
+//     console.log('Hello');
+// };
+// logger();
+
+
+// Стрелочные функции () => - Не имеет своего контекста (this)
+
+// const calk = (a, b) => a + b;
+
+// const pussy = (a, b) => {
+//     console.log('a');
+//     return a + b;
+// };
+
+
+//Аргументы функции 
+
+// const usdCurr = 28;
+// const discount = 0.9;
+
+
+// function convert(amount, curr) {
+//     return curr * amount;
+// }
+
+
+// function promotion(result) {
+//     console.log(result * discount);
+// }
+
+// const res = convert(500, usdCurr);
+// promotion(res);
+
+
+// function test() {
+//     for (let i = 0; i < 5; i++) {
+//             console.log(i);
+//             if (i === 3) return
+//         }
+//             console.log('Done');
+// }
+
+// test();
+
+// function doNothing() {};
+//     console.log(doNothing() === undefined);
+
+
+// // Методы и свойства у строк и чисел:
+// // Методы - это вспомогательные функции;
+// // Свойства- это вспомогательные значения;
+
+
+// const str = "teSt";
+
+// // console.log(str.length);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str);
+
+// const fruit = "Some fruit";
+// console.log(fruit.indexOf("fruit"));
+
+// const logg = "Hello world";
+
+// console.log(logg.slice(6, 11));
+
+// console.log(logg.substring(6, 11));
+
+
+
+
+// const bigger = "pyssu jussy nou hi mother";
+ 
+// console.log(bigger.slice(16, 18));
+
+
+// const bigger_1 = "pyssu jussy nou hi mother";
+ 
+// console.log(bigger_1.slice(16, 18));
+
+// const nue = "bigger bigger";
+// console.log(nue.substr(6, 5));
+
+
+// const num_1 = 12.2;
+
+// console.log(Math.round(num_1));
+
+
+// const test = "12.2px";
+// console.log(parseInt(test)); 
+
+
+
+/* Задание на урок:
+
+1) Первую часть задания посторить по уроку
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции false - выводит в консоль главный объект программы;
+3) Создать функцию writeYourGenres в которой цпользователь будет 3 раза отвечать на вопрос "Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данныз genres
+
+*/
+
+"use strict";
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -169,25 +320,66 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const film = prompt('Один из последних просмотренных фильмов?', ''),
+              score = prompt('На сколько оцените его?', '');
+
+        const isFilmValid = (film != null && film != '' && film.length < 50);
+        const isScoreValid = score != null  && score != '';
+
+        if (isFilmValid && isScoreValid) {
+            personalMovieDB.movies[film] = score;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+
+        }
     }
 }
 
-if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log ("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-    console.log('Вы киноман');
-} else {
-    console.log('Произошла ошибка');
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log ("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
 }
+
+
+detectPersonalLevel();
+
+
+
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+        personalMovieDB.genres[i - 1] = genre;
+
+    }
+}
+
+writeYourGenres();
+
+
+
+
+
