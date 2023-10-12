b2e1200de17411e99027087fa26bee8eb1bdba37
Raman <roma.milashevich@gmail.com>
2023-10-12 10:27:48 +0200

    Dinamic types in JS 12.10.2023, 10:27

---
+++ src/js/test.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function hello() {
+    console.log("Hello world");
+}
+hello();
+
+function hi() {
+    console.log('Say hi!');
+}
+hi();
+
+const arr = [1, 14, 4, 30, 54];
+const sorted = arr.sort(compareNum);
+
+function compareNum(a, b) {
+    return a - b;
+}
+
+console.log(sorted);
